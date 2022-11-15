78a7bae44a9824606e8b3851d953a12831f9d7bd
wavey0x <retrowaveyy@gmail.com>
2022-11-15 13:55:23 -0500

    chore: add locked profit test

--- tests/test_share_value.py
+++ tests/test_share_value.py
@@ -1,8 +1,8 @@
-from brownie import Contract, accounts, chain, ShareValueHelper
+from brownie import Contract, accounts, chain, ShareValueHelper, web3
 import pytest
 
 def test_helper(helper):
-    vault = Contract('0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE')
+    vault = Contract('0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE', owner=web3.ens.resolve('ychad.eth'))
     decimals = vault.decimals()
     amount = 1_000_000 * 10 ** decimals
     
@@ -19,3 +19,39 @@ def test_helper(helper):
     print(f'\n-- Converting {amount} shares to underlying amount --')
     print(f'Precise: {amount_precise}')
     print(f'Imprecise: {amount_imprecise}')
+
+
+    # Harvest a strategy so we can test during a locked profit scenario
+
+
+    strategy = Contract('0x342491C093A640c7c2347c4FFA7D8b9cBC84D1EB', owner=web3.ens.resolve('ychad.eth'))
+    assert strategy.estimatedTotalAssets() > 10**decimals
+    strategy.harvest()
+    pps = vault.pricePerShare()
+    chain.sleep(60*60)
+    chain.mine()
+    assert pps < vault.pricePerShare()
+
+    amount = 10_000e6
+    tx = vault.deposit(amount)
+    imprecise = amount * 10**decimals / vault.pricePerShare()
+    expected = helper.amountToShares(vault, amount) # Must check this after the deposit so that calc is done at same block
+    actual = tx.return_value
+    print(f'\nExpected (imprecise): {imprecise}')
+    print(f'Expected (precise): {expected}')
+    print(f'Actual: {actual}')
+    assert expected == actual
+
+    chain.sleep(60*60)
+    chain.mine()
+    amount = expected
+    tx = vault.withdraw(amount)
+    expected = helper.sharesToAmount(vault, amount)
+    imprecise = amount * vault.pricePerShare() / 10**decimals
+    actual = tx.return_value
+    print(f'\nExpected (imprecise): {imprecise}')
+    print(f'Expected (precise): {expected}')
+    print(f'Actual: {actual}')
+    assert expected == actual
+    assert tx.return_value == expected
+
